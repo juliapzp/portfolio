12409382b5df78b80dccdaaa34eed27db25081bd
Júlia Zanin Pereira <127359841+juliapzp@users.noreply.github.com>
2024-03-26 11:32:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 function showSlide(slideNumber) {
-    var slides = document.getElementsByClassName("slide");
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("active");
-    }
-    slides[slideNumber - 1].classList.add("active");
+  var slides = document.getElementsByClassName("slide");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[slideNumber - 1].classList.add("active");
 }
 
-  document.getElementById("btnsobre").addEventListener("click",function(){ 
-    document.getElementById("sobre").scrollIntoView({
-      behavior: 'smooth'
-    })
-
-  })
-
-  window.addEventListener('DOMContentLoaded', function() {
-    var sobre = document.getElementById('sobre');
-    var fundo = document.getElementById('fundo');
-  
-    function BackgroundHeight() {
-      if (sobre && fundo) {
-        var sobreHeight = sobre.offsetHeight;
-        fundo.style.height = sobreHeight + 'px';
-      }
-    }
-  
-    BackgroundHeight();
-  
-    window.addEventListener('resize', function() {
-      BackgroundHeight();
-    });
+document.getElementById("btnsobre").addEventListener("click", function() {
+  document.getElementById("sobre").scrollIntoView({
+    behavior: 'smooth'
   });
+});
+
+document.getElementById("btntec").addEventListener("click", function() {
+  document.getElementById("tecnico").scrollIntoView({
+    behavior: 'smooth'
+  });
+});
+
+document.getElementById("btnmat").addEventListener("click", function() {
+  document.getElementById("trabalhos").scrollIntoView({
+    behavior: 'smooth'
+  });
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+  var containerText = document.querySelector('.container-text');
+  var fundo = document.getElementById('fundo');
+
+  function BackgroundHeight() {
+    if (containerText && fundo) {
+      var containerHeight = containerText.offsetHeight;
+      fundo.style.height = containerHeight + 'px';
+    }
+  }
+
+  BackgroundHeight();
+
+  window.addEventListener('resize', function() {
+    BackgroundHeight();
+  });
+});
